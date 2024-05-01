4d82f6d1d4944653580e897fad0cedf67411a500
Mustafa Mehmedoglu <mikbalm@gmail.com>
2024-04-30 22:42:57 -0400

    lab complete

--- index.js
+++ index.js
@@ -1,13 +1,32 @@
 function isPalindrome(word) {
   // Write your algorithm here
+
+  let originalStringArray = word.split('');
+  originalStringArray.reverse();
+  let reversedString= originalStringArray.join('');
+
+  return word === reversedString;
 }
 
 /* 
   Add your pseudocode here
+
+  create an array from the original word string
+  use reverse() on created array
+  make a string from the reversed array
+  comapare original word string with reversed string
 */
 
 /*
   Add written explanation of your solution here
+  
+  since all strings in JS behave like arrays
+  best approach would be to see if the reverse
+  of any given string is the same with the original
+  using .reverse() built in function on array form
+  of said string, we can return the boolean value by
+  comparing the original word string with the 
+  reversed one.
 */
 
 // You can run `node index.js` to view these console logs
@@ -20,6 +39,16 @@ if (require.main === module) {
 
   console.log("Expecting: false");
   console.log("=>", isPalindrome("robot"));
+
+  console.log("");
+
+  console.log("Expecting: true");
+  console.log("=>", isPalindrome("madam"));
+
+  console.log("");
+
+  console.log("Expecting: false");
+  console.log("=>", isPalindrome("salts"));
 }
 
 module.exports = isPalindrome;
